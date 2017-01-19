yuq/mesa-lima

commit 63c58dfc653c499aab5b8d0ea07f1dc1af88c856

android: set HAVE_FUNC_ATTRIBUTE_ALIAS
Analogous to previous two commits.

Strictly speaking it's not be applicable for Android since we don't
build GLX and related code.

Regardless keep things consistent with the other build systems.

Cc: Rob Herring <robh@kernel.org>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

Android.common.mk
@@ -51,6 +51,7 @@ LOCAL_CFLAGS += \
 	-DHAVE_FUNC_ATTRIBUTE_UNUSED \
 	-DHAVE_FUNC_ATTRIBUTE_FORMAT \
 	-DHAVE_FUNC_ATTRIBUTE_PACKED \
+	_DHAVE_FUNC_ATTRIBUTE_ALIAS \
 	-DHAVE___BUILTIN_CTZ \
 	-DHAVE___BUILTIN_POPCOUNT \
 	-DHAVE___BUILTIN_POPCOUNTLL \

intel/external-mesa

commit b967231b73138efb3412e7f5eb202a8a80ddc852

android: correct typo in build
Fixes: 63c58dfc653c499aab5b8d0ea07f1dc1af88c856
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

Android.common.mk
@@ -52,7 +52,7 @@ LOCAL_CFLAGS += \
 	-DHAVE_FUNC_ATTRIBUTE_UNUSED \
 	-DHAVE_FUNC_ATTRIBUTE_FORMAT \
 	-DHAVE_FUNC_ATTRIBUTE_PACKED \
-	_DHAVE_FUNC_ATTRIBUTE_ALIAS \
+	-DHAVE_FUNC_ATTRIBUTE_ALIAS \
 	-DHAVE___BUILTIN_CTZ \
 	-DHAVE___BUILTIN_POPCOUNT \
 	-DHAVE___BUILTIN_POPCOUNTLL \