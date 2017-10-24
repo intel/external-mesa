650024e91b4cc1cbae57100db88966acb809d03c
Tapani Pälli <tapani.palli@intel.com>
2017-10-24 15:06:37 +0300

    AndroidIA: glsl runtime modification infrastructure
    This infrastructure can be used for debugging shaders.
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

---
+++ src/mesa/main/glsl_modifications.c
@@ -0,0 +1,11 @@
+
+   {
+      "7b7cb322a1f2d2aadfe7299c8ccf336a2f48dd37",
+      {
+         {
+            INSERT,
+            63,
+            "	if (motion.xy == vec2(0.5) || texel.a == 0.0) { gl_FragColor = texel; return; }",
+         },
+      },
+   },
