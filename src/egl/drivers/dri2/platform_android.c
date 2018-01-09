4f390ab9b23446e22541ecbf85bb793a895f3257
Tomasz Figa <tfiga@chromium.org>
2018-01-09 13:32:38 +0200

    CHROMIUM: egl/android: Support opening render nodes from within EGL
    This patch adds support for opening render nodes directly from within
    display initialization, Instead of relying on private interfaces
    provided by gralloc.
    
    In addition to having better separation from gralloc and being able to
    use different render nodes for allocation and rendering, this also fixes
    problems encountered when using the same DRI FD for gralloc and Mesa,
    when both stepped each over another because of shared GEM handle
    namespace.
    
    BUG=b:29036398
    TEST=No significant regressions in dEQP inside the container
    
    Change-Id: I7f901eb9dadbfc2200484666fdc6a2bc0ca42a0c
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/367215
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -37,7 +37,7 @@
 #include "loader.h"
 #include "egl_dri2.h"
 #include "egl_dri2_fallbacks.h"
-#include "gralloc_drm.h"
+#include "platform_android_gralloc_drm.h"
 
 #define ALIGN(val, align)	(((val) + (align) - 1) & ~((align) - 1))
 
@@ -1124,11 +1124,51 @@ droid_open_device(struct dri2_egl_display *dri2_dpy)
                                           GRALLOC_MODULE_PERFORM_GET_DRM_FD,
                                           &fd);
    if (err || fd < 0) {
-      _eglLog(_EGL_WARNING, "fail to get drm fd");
+      _eglLog(_EGL_DEBUG, "fail to get drm fd");
       fd = -1;
    }
 
    return (fd >= 0) ? fcntl(fd, F_DUPFD_CLOEXEC, 3) : -1;
+}
+
+#define DRM_RENDER_DEV_NAME  "%s/renderD%d"
+
+static int
+droid_probe_device(_EGLDisplay *dpy)
+{
+   struct dri2_egl_display *dri2_dpy = dpy->DriverData;
+   const int limit = 64;
+   const int base = 128;
+   int fd;
+   int i;
+
+   for (i = 0; i < limit; ++i) {
+      char *card_path;
+      if (asprintf(&card_path, DRM_RENDER_DEV_NAME, DRM_DIR_NAME, base + i) < 0)
+         continue;
+
+      fd = loader_open_device(card_path);
+      free(card_path);
+      if (fd < 0)
+         continue;
+
+      dri2_dpy->driver_name = loader_get_driver_for_fd(fd);
+      if (!dri2_dpy->driver_name) {
+         close(fd);
+         continue;
+      }
+
+      dri2_dpy->fd = fd;
+      if (dri2_load_driver(dpy))
+         return 0;
+
+      close(fd);
+      dri2_dpy->fd = -1;
+      free(dri2_dpy->driver_name);
+      dri2_dpy->driver_name = NULL;
+   }
+
+   return -1;
 }
 
 static const struct dri2_egl_display_vtbl droid_display_vtbl = {
@@ -1215,14 +1255,19 @@ dri2_initialize_android(_EGLDriver *drv, _EGLDisplay *disp)
    disp->DriverData = (void *) dri2_dpy;
 
    dri2_dpy->fd = droid_open_device(dri2_dpy);
-   if (dri2_dpy->fd < 0) {
-      err = "DRI2: failed to open device";
-      goto cleanup;
-   }
+   if (dri2_dpy->fd >= 0) {
+      dri2_dpy->driver_name = loader_get_driver_for_fd(dri2_dpy->fd);
+      if (dri2_dpy->driver_name == NULL) {
+         err = "DRI2: failed to get driver name";
+         goto cleanup;
+      }
 
-   dri2_dpy->driver_name = loader_get_driver_for_fd(dri2_dpy->fd);
-   if (dri2_dpy->driver_name == NULL) {
-      err = "DRI2: failed to get driver name";
+      if (!dri2_load_driver(disp)) {
+         err = "DRI2: failed to load driver";
+         goto cleanup;
+      }
+   } else if (droid_probe_device(disp) < 0) {
+      err = "DRI2: failed to open device";
       goto cleanup;
    }
 
