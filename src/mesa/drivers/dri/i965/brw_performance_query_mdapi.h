1e3dd150d3d69ff1b94c06e6889aa35bb2d9d3e0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2018-04-03 15:48:13 +0100

    FROMLIST: i965: perf: enable GPA query statistics
    The combinaison of GPA/MDAPI components expects a particular name &
    layout for their pipeline statistics query.
    
    v2: Limit the query GPA/MDAPI statistics to gen7->9 (Lionel)
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/mesa/drivers/dri/i965/brw_performance_query_mdapi.h
+++ src/mesa/drivers/dri/i965/brw_performance_query_mdapi.h
@@ -34,6 +34,6 @@ int brw_perf_query_get_mdapi_oa_data(struct brw_context *brw,
                                      uint8_t *data);
 
 void brw_perf_query_register_mdapi_oa_query(struct brw_context *brw);
-
+void brw_perf_query_register_mdapi_statistic_query(struct brw_context *brw);
 
 #endif /* BRW_PERFORMANCE_QUERY_MDAPI_H */
