d6b8e885fe70bc8bf677eb321ad51f8c4de5aa40
valtteri rantala <valtteri.rantala@intel.com>
2018-05-18 11:31:12 +0300

    Forces color writes to simd32. Original patch from Toni Lönnberg
    Change-Id: I9fcac17e609b8647e786f9bd81ec9246b167bbac

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -7170,9 +7170,9 @@ brw_compile_fs(const struct brw_compiler *compiler, void *log_data,
 
    if (v8.max_dispatch_width >= 32 && !use_rep_send &&
        compiler->devinfo->gen >= 6 &&
-       (INTEL_DEBUG & DEBUG_DO32 ||
-        strncmp(src_shader->info.name, "BLORP", 5) == 0)) {
-      /* Try a SIMD32 compile */
+       strncmp(src_shader->info.name, "BLORP", 5) == 0 ||
+       shader->info.outputs_written <= BITFIELD64_BIT(FRAG_RESULT_DATA0)) {
+       /* Try a SIMD32 compile */
       fs_visitor v32(compiler, log_data, mem_ctx, key,
                      &prog_data->base, prog, shader, 32,
                      shader_time_index32);
