101ff2790ef17c43b951dc7f473ca78ceab1be92
renchenglei <chenglei.ren@intel.com>
2019-03-12 16:28:30 +0800

    REVERTME: anv/android: disable couple of vulkan extensions for Android CTS
    This patch is similar to c9a5ddb0e88b8be24f523a1ca6fae61637e0ffe5,
    3 more new extensions cause CTS fail due to mesa rebase to 18.3.2,
    we disable them to pass following two CTS cases.
    
    dEQP-VK.api.info.device.extensions
    dEQP-VK.api.info.android#no_unknown_extensions
    
    Signed-off-by: Chenglei Ren <chenglei.ren@intel.com>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -78,7 +78,7 @@ EXTENSIONS = [
     Extension('VK_KHR_descriptor_update_template',        1, True),
     Extension('VK_KHR_device_group',                      1, True),
     Extension('VK_KHR_device_group_creation',             1, True),
-    Extension('VK_KHR_driver_properties',                 1, True),
+    Extension('VK_KHR_driver_properties',                 1, '!ANDROID'),
     Extension('VK_KHR_external_fence',                    1,
               'device->has_syncobj_wait'),
     Extension('VK_KHR_external_fence_capabilities',       1, True),
@@ -128,8 +128,8 @@ EXTENSIONS = [
     Extension('VK_EXT_post_depth_coverage',               1, 'device->info.gen >= 9'),
     Extension('VK_EXT_sampler_filter_minmax',             1, 'device->info.gen >= 9'),
     Extension('VK_EXT_calibrated_timestamps',             1, True),
-    Extension('VK_GOOGLE_decorate_string',                1, True),
-    Extension('VK_GOOGLE_hlsl_functionality1',            1, True),
+    Extension('VK_GOOGLE_decorate_string',                1, '!ANDROID'),
+    Extension('VK_GOOGLE_hlsl_functionality1',            1, '!ANDROID'),
 ]
 
 class VkVersion:
